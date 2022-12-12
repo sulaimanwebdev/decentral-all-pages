6a0157b29f8d1e12359ecba31115ccfd0cc0c004
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-12-13 01:29:16 +0500

    relavent search completed

--- src/components/shared/FriendsCompMobile.js
+++ src/components/shared/FriendsCompMobile.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom'
 
-const Friends = () => {
+const Friends = (props) => {
 
     let data = [
         {
@@ -184,7 +184,10 @@ const Friends = () => {
        <div className="sidebar-scroll h-full relative bg-transparent-main-color overflow-y-auto lg4:max-h-screen my-5 rounded-xl">
         <div className='h-[calc(100%-100px)]'>
 
-             <div className='my-8 mt-3 pt-5 bg-white rounded-xl'>
+             {
+                props.relavent === "" ?
+                <div>
+                <div className='my-8 mt-0 pt-5 bg-white rounded-xl'>
 
                 <div className="px-5 lg4:px-4 flex items-center justify-between border-b pb-3 border-[#F9F9F9]">
                     <div className='text-[17px] text-[#11047A] font-bold'>Friends Request</div>
@@ -223,7 +226,7 @@ const Friends = () => {
 
 
 
-             <div className='pt-5 bg-white rounded-xl'>
+<div className='pt-5 bg-white rounded-xl'>
 
 <div className="px-5 lg4:px-4 flex items-center justify-between border-b pb-3 border-[#F9F9F9]">
     <div className='text-[17px] text-[#11047A] font-bold'>Suggestion People</div>
@@ -259,6 +262,56 @@ const Friends = () => {
                 </div>
 
 </div>
+</div>
+
+
+:
+
+
+<div className='my-8 mt-0 pt-5 bg-white rounded-xl'>
+
+<div className="px-5 lg4:px-4 flex items-center justify-between border-b pb-3 border-[#F9F9F9]">
+    <div className='text-[17px] text-[#11047A] font-bold'>Relavent Search</div>
+    <button><svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.2175 11.3629C14.1286 11.3629 12.4342 13.072 12.4342 15.1815C12.4342 17.2898 14.1286 19 16.2175 19C18.3064 19 19.9997 17.2898 19.9997 15.1815C19.9997 13.072 18.3064 11.3629 16.2175 11.3629Z" fill="#0085FF"/><path opacity="0.4" d="M8.08337 13.6605H1.50788C0.67567 13.6605 0.00012207 14.3424 0.00012207 15.1812C0.00012207 16.0201 0.67567 16.703 1.50788 16.703H8.08337C8.91447 16.703 9.59002 16.0201 9.59002 15.1812C9.59002 14.3424 8.91447 13.6605 8.08337 13.6605Z" fill="#0085FF"/><path d="M3.78329 7.6371C5.87216 7.6371 7.56658 5.928 7.56658 3.81967C7.56658 1.71022 5.87216 0 3.78329 0C1.69443 0 0 1.71022 0 3.81967C0 5.928 1.69443 7.6371 3.78329 7.6371Z" fill="#0085FF"/><path opacity="0.4" d="M20 3.81927C20 2.98043 19.3244 2.29858 18.4933 2.29858H11.9179C11.0856 2.29858 10.4101 2.98043 10.4101 3.81927C10.4101 4.65812 11.0856 5.33997 11.9179 5.33997H18.4933C19.3244 5.33997 20 4.65812 20 3.81927Z" fill="#0085FF"/></svg></button>
+</div>
+
+
+<div className={`sidebar-scroll relative flex flex-col divide-y divide-[#F9F9F9] h-full ${overflow} ${overflow === "overflow-y-hidden" ? "lg4:max-h-[50vh]" : "max-h-[600px]"} `}>
+{
+  data.slice(0, 4).map((ele)=>{
+    return(
+        <div key={Math.random()} className="flex sm3:items-center justify-between flex-col sm3:flex-row gap-2 py-4 px-5 lg4:px-4">
+<div className="flex items-center gap-3">
+<img src="/images/profile.png" className='w-[60px]' alt="profile" />
+<div className='text-left flex flex-col gap-1'>
+<div className='oneLineText text-[#11047A] text-[15px] font-bold'>{ele.name}</div>
+<div className='oneLineText text-not-black text-[15px]'>{ele.id}</div>
+</div>
+</div>
+
+<div className="grid grid-cols-2 sm3:flex sm3:items-end sm3:justify-end w-full sm3:w-auto gap-2 mt-3 sm3:mt-0">
+<button className='bg-main-color rounded-lg text-white px-3.5 h-[43px] flex items-center justify-center text-[14px]'>Dismiss</button>
+<button className='bg-[#FF21211A] rounded-lg text-[#FF3535] px-3.5 h-[43px] flex items-center justify-center text-[14px]'>Accept</button>
+</div>
+
+</div>
+    )
+  })
+}
+
+
+</div>
+
+</div>
+
+              }
+
+             
+
+
+
+
+             
 
 
 
--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -90,6 +90,7 @@ const Friends = () => {
   
   const [show, setshow] = React.useState('Friends');
   const [friends, setfriends] = React.useState(false);
+  const [relavent, setrelavent] = React.useState('');
 
   React.useEffect(() => {
     document.body.classList.add('dashboard');
@@ -118,7 +119,7 @@ const Friends = () => {
              <button onClick={()=> {setshow("Friends")}} className={`${show === "Friends" ? "sm2:px-6 py-2.5 rounded-xl text-[13px] sm5:text-[15px] bg-main-color text-white" : "sm2:px-6 py-2.5 rounded-xl text-[13px] sm5:text-[15px] bg-transparent-main-color text-not-black"}`}>Friends</button>
              <button onClick={()=> {setshow("Add New")}} className={`${show === "Add New" ? "sm2:px-6 py-2.5 rounded-xl text-[13px] sm5:text-[15px] bg-main-color text-white" : "sm2:px-6 py-2.5 rounded-xl text-[13px] sm5:text-[15px] bg-transparent-main-color text-not-black"}`}>Add New</button>
           </div>
-          <div className="w-full lg4:w-[400px] flex items-center gap-2 bg-white border border-[#F5F5F7] focus-within:border-[#0085FF] focus-within:bg-transparent-main-color  rounded-xl px-3"><input type="text" placeholder='Search Recent Friends' className='w-full bg-transparent py-3 text-[#11047A] text-[15px] border-none outline-none' /><svg width="22" height="22" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.625 15.75C12.56 15.75 15.75 12.56 15.75 8.625C15.75 4.68997 12.56 1.5 8.625 1.5C4.68997 1.5 1.5 4.68997 1.5 8.625C1.5 12.56 4.68997 15.75 8.625 15.75Z" stroke="#11047A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M16.5 16.5L15 15" stroke="#11047A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
+          <div className="w-full lg4:w-[400px] flex items-center gap-2 bg-white border border-[#F5F5F7] focus-within:border-[#0085FF] focus-within:bg-transparent-main-color  rounded-xl px-3"><input type="text" onChange={(e)=> {setrelavent(e.target.value)}} value={relavent} placeholder='Search Recent Friends' className='w-full bg-transparent py-3 text-[#11047A] text-[15px] border-none outline-none' /><svg width="22" height="22" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.625 15.75C12.56 15.75 15.75 12.56 15.75 8.625C15.75 4.68997 12.56 1.5 8.625 1.5C4.68997 1.5 1.5 4.68997 1.5 8.625C1.5 12.56 4.68997 15.75 8.625 15.75Z" stroke="#11047A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M16.5 16.5L15 15" stroke="#11047A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
       </div>
 
       
@@ -137,6 +138,8 @@ const Friends = () => {
              </div>
 
         :
+        relavent === ""
+        ?
         <div className="flex flex-col divide-y divide-[#F9F9F9]">
       {
         data.map((ele)=>{
@@ -162,12 +165,48 @@ const Friends = () => {
         })
       }
       </div>
+      :
+      
+      <div>
+        <div className="flex items-center justify-between border-b pb-3 border-[#F9F9F9]">
+    <div className='text-[17px] text-[#11047A] font-bold'>Relavent Search</div>
+    <button><svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.2175 11.3629C14.1286 11.3629 12.4342 13.072 12.4342 15.1815C12.4342 17.2898 14.1286 19 16.2175 19C18.3064 19 19.9997 17.2898 19.9997 15.1815C19.9997 13.072 18.3064 11.3629 16.2175 11.3629Z" fill="#0085FF"/><path opacity="0.4" d="M8.08337 13.6605H1.50788C0.67567 13.6605 0.00012207 14.3424 0.00012207 15.1812C0.00012207 16.0201 0.67567 16.703 1.50788 16.703H8.08337C8.91447 16.703 9.59002 16.0201 9.59002 15.1812C9.59002 14.3424 8.91447 13.6605 8.08337 13.6605Z" fill="#0085FF"/><path d="M3.78329 7.6371C5.87216 7.6371 7.56658 5.928 7.56658 3.81967C7.56658 1.71022 5.87216 0 3.78329 0C1.69443 0 0 1.71022 0 3.81967C0 5.928 1.69443 7.6371 3.78329 7.6371Z" fill="#0085FF"/><path opacity="0.4" d="M20 3.81927C20 2.98043 19.3244 2.29858 18.4933 2.29858H11.9179C11.0856 2.29858 10.4101 2.98043 10.4101 3.81927C10.4101 4.65812 11.0856 5.33997 11.9179 5.33997H18.4933C19.3244 5.33997 20 4.65812 20 3.81927Z" fill="#0085FF"/></svg></button>
+</div>
+      <div className="flex flex-col divide-y divide-[#F9F9F9]">
+      {
+        data.slice(0, 4).map((ele)=>{
+            return(
+                <div key={Math.random()} className="flex sm10:items-center justify-between flex-col sm10:flex-row py-4 gap-3">
+      <div className="flex items-center gap-3">
+         <img src="/images/profile.png" className='w-[60px]' alt="profile" />
+         <div className='text-left flex flex-col gap-1'>
+             <div className='oneLineText text-[#11047A] text-[17px] font-bold'>{ele.name}</div>
+             <div className='oneLineText text-not-black text-[15px]'>{ele.userName}</div>
+         </div>
+      </div>
+
+      <div className="flex justify-evenly sm8:grid sm8:grid-cols-4 sm10:flex items-center sm10:justify-center w-full sm5:w-auto gap-2">
+        <button className='bg-main-color rounded-lg text-white px-3.5 h-[43px] flex items-center justify-center text-[15px]'>Request</button>
+        <button className='bg-transparent-main-color rounded-lg text-main-color px-3.5 h-[43px] flex items-center justify-center text-[15px]'>Send</button>
+        <Link to="/chat"><div className='bg-white border border-[#F5F5F7] rounded-lg text-main-color px-2.5 h-[43px] flex items-center justify-center'><svg width="23" height="23" className='translate-y-[1px]' viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.99984 19.0083C9.42484 19.0083 8.88317 18.7166 8.49984 18.2083L7.24984 16.5416C7.22484 16.5083 7.12484 16.4666 7.08317 16.4583H6.6665C3.1915 16.4583 1.0415 15.5166 1.0415 10.8333V6.66663C1.0415 2.98329 2.98317 1.04163 6.6665 1.04163H13.3332C17.0165 1.04163 18.9582 2.98329 18.9582 6.66663V10.8333C18.9582 14.5166 17.0165 16.4583 13.3332 16.4583H12.9165C12.8498 16.4583 12.7915 16.4916 12.7498 16.5416L11.4998 18.2083C11.1165 18.7166 10.5748 19.0083 9.99984 19.0083ZM6.6665 2.29163C3.68317 2.29163 2.2915 3.68329 2.2915 6.66663V10.8333C2.2915 14.6 3.58317 15.2083 6.6665 15.2083H7.08317C7.50817 15.2083 7.9915 15.45 8.24984 15.7916L9.49984 17.4583C9.7915 17.8416 10.2082 17.8416 10.4998 17.4583L11.7498 15.7916C12.0248 15.425 12.4582 15.2083 12.9165 15.2083H13.3332C16.3165 15.2083 17.7082 13.8166 17.7082 10.8333V6.66663C17.7082 3.68329 16.3165 2.29163 13.3332 2.29163H6.6665Z" fill="#0085FF"/><path d="M9.99984 10C9.53317 10 9.1665 9.62504 9.1665 9.16671C9.1665 8.70837 9.5415 8.33337 9.99984 8.33337C10.4582 8.33337 10.8332 8.70837 10.8332 9.16671C10.8332 9.62504 10.4665 10 9.99984 10Z" fill="#0085FF"/><path d="M13.3333 10C12.8667 10 12.5 9.62504 12.5 9.16671C12.5 8.70837 12.875 8.33337 13.3333 8.33337C13.7917 8.33337 14.1667 8.70837 14.1667 9.16671C14.1667 9.62504 13.8 10 13.3333 10Z" fill="#0085FF"/><path d="M6.66683 10C6.20016 10 5.8335 9.62504 5.8335 9.16671C5.8335 8.70837 6.2085 8.33337 6.66683 8.33337C7.12516 8.33337 7.50016 8.70837 7.50016 9.16671C7.50016 9.62504 7.1335 10 6.66683 10Z" fill="#0085FF"/></svg></div></Link>
+        <button className='bg-[#FF21211A] rounded-lg text-main-color px-2.5 h-[43px] flex items-center justify-center'><svg width="18" height="20" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.9059 3.36934C15.2301 3.36934 15.5 3.63851 15.5 3.981V4.29767C15.5 4.63183 15.2301 4.90933 14.9059 4.90933H1.09488C0.769883 4.90933 0.5 4.63183 0.5 4.29767V3.981C0.5 3.63851 0.769883 3.36934 1.09488 3.36934H3.52464C4.01821 3.36934 4.44775 3.01851 4.55879 2.52352L4.68603 1.95519C4.88378 1.18103 5.53458 0.66687 6.27939 0.66687H9.72061C10.4573 0.66687 11.1154 1.18103 11.3059 1.91436L11.442 2.52268C11.5522 3.01851 11.9818 3.36934 12.4762 3.36934H14.9059ZM13.6718 14.9449C13.9255 12.5808 14.3696 6.9642 14.3696 6.90753C14.3858 6.73586 14.3299 6.57337 14.2188 6.44253C14.0997 6.32004 13.949 6.24754 13.7828 6.24754H2.22403C2.05708 6.24754 1.89823 6.32004 1.788 6.44253C1.67616 6.57337 1.62105 6.73586 1.62915 6.90753C1.63064 6.91794 1.64658 7.11578 1.67322 7.44654C1.79158 8.9159 2.12124 13.0084 2.33426 14.9449C2.485 16.3716 3.42108 17.2683 4.77698 17.3008C5.82329 17.3249 6.9012 17.3333 8.00343 17.3333C9.04163 17.3333 10.096 17.3249 11.1748 17.3008C12.5777 17.2766 13.5129 16.3958 13.6718 14.9449Z" fill="#FF2121"/></svg></button>
+      </div>
+
+      </div>
+            )
+        })
+      }
+      </div>
+      </div>
+
+
+
       }
 
       </div>
 
 
-      <div className={`${show === "Add New" ? "block" : "hidden"} lg4:hidden mt-5`}><FriendsCompMobile/></div>
+      <div className={`${show === "Add New" ? "block" : "hidden"} lg4:hidden mt-5`}><FriendsCompMobile relavent={relavent}/></div>
 
 
       </div>
